8b3d1a9e5da10ef5b08cc13ec909f5580b9e486f
GitHub Action <action@github.com>
2023-12-22 15:49:32 +0000

    Bump version to v0.0.14

---
+++ Formula/mycliexample.rb
@@ -0,0 +1,29 @@
+class Mycliexample < Formula
+  version "0.0.14"  # Replace with the actual version number
+  desc "Example of releasing a CLI into brew, using GitOps principles"
+  homepage "https://github.com/qcastel/test-cli-release"
+  url "https://github.com/qcastel/test-cli-release/releases/latest/download/release.tar.gz"
+
+  def install
+    # Install the executable to bin
+    bin.install "mycliexample"
+
+    # Ensure execute permissions are set
+    chmod "+x", "#{bin}/mycliexample"
+
+    # Create the user data directory
+    (etc/"mycliexample").mkpath
+
+    # Copy everything under lib to /usr/local/lib/mycliexample
+    (lib/"mycliexample").install "version.txt", "lib"
+
+    # Install the Zsh autocompletion file
+    zsh_completion.install "autocompletions/mycliexample.zsh"
+
+  end
+
+  test do
+    # Your test logic here
+    system "#{bin}/mycliexample"
+  end
+end
