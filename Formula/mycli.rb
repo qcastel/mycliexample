4d09752ce2610ec1b4b6bfab931c086888ea820d
Quentin Castel <quentin.castel@dataiku.com>
2023-12-22 16:19:35 +0100

    Fix

--- Formula/mycli.rb
+++ Formula/mycli.rb
@@ -1,22 +1,25 @@
 class Mycli < Formula
-  version "0.0.0"
+  version "1.27.0"  # Replace with the actual version number
   desc "Example of releasing a CLI into brew, using GitOps principles"
   homepage "https://github.com/qcastel/test-cli-release"
   url "https://github.com/qcastel/test-cli-release/releases/latest/download/release.tar.gz"
 
   def install
+    # Install the executable to bin
     bin.install "mycli"
 
-    # Set correct permissions for the mycli executable
+    # Ensure execute permissions are set
     chmod "+x", "#{bin}/mycli"
 
     # Create the user data directory
     (etc/"mycli").mkpath
 
-    lib.install "autocompletions", "version.txt", "lib"
+    # Copy everything under lib to /usr/local/lib/mycli
+    (lib/"mycli").install "version.txt", "lib"
 
     # Install the Zsh autocompletion file
     zsh_completion.install "autocompletions/mycli.zsh"
+
   end
 
   test do
