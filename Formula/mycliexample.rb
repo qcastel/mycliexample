86173442861d0e2cb5b33eab7f34b5b0a2377da4
GitHub Action <action@github.com>
2023-12-23 14:13:51 +0000

    Bump version to v0.0.23

--- Formula/mycliexample.rb
+++ Formula/mycliexample.rb
@@ -1,12 +1,12 @@
 class Mycliexample < Formula
-  version "0.0.22"  # Replace with the actual version number
-  desc "Example of releasing a CLI with brew, using GitOps principles on a github repository"
+  version "v0.0.23"
+  desc "Example of releasing a CLI into brew, using GitOps principles"
   homepage "https://github.com/qcastel/test-cli-release"
-  url "https://github.com/qcastel/test-cli-release/releases/latest/download/release.tar.gz"
+  url "git@github.com:dataiku/dataiku-platform-tooling.git", :using => :git, :tag => 'v0.0.23'
 
   def install
     # Install the executable to bin
-    bin.install "mycliexample"
+    bin.install "mycliexample_brew" => "mycliexample"
 
     # Ensure execute permissions are set
     chmod "+x", "#{bin}/mycliexample"
@@ -18,7 +18,7 @@ class Mycliexample < Formula
     (lib/"mycliexample").install "version.txt", "lib"
 
     # Install the Zsh autocompletion file
-    zsh_completion.install "autocompletions/mycliexample.zsh"
+    zsh_completion.install "autocompletions/mycliexample.zsh"  => "_mycliexample"
 
   end
 
