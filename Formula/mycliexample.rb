6b5310e0503e31d5318651f6948888f63b3c8d5d
Quentin Castel <quentincastel86@gmail.com>
2023-12-23 15:43:55 +0100

    try fixing brew formula

--- Formula/mycliexample.rb
+++ Formula/mycliexample.rb
@@ -1,11 +1,12 @@
 #READ ONLY! EDIT TEMPLATE INSTEAD. THIS FILE WILL BE OVERRIDED AT THE NEXT RELEASE
 class Mycliexample < Formula
-  version "v0.0.28"
+  version "v0.0.27"
   desc "Example of releasing a CLI into brew, using GitOps principles"
   homepage "https://github.com/qcastel/mycliexample"
-  url "git@github.com:qcastel/mycliexample.git",
-      tag:      "v0.0.28",
-      revision: "v0.0.28"
+  url "git@github.com:dataiku/dataiku-platform-tooling.git",
+      using: :git
+      tag:      "v0.0.27",
+      revision: "v0.0.27"
 
   def install
     # Install the executable to bin
